14943046d617ebf68c2f3905b2b26d14a37f1f2c
Elpra <sebastianprat11@gmail.com>
2018-10-14 16:54:33 -0300

    Form console

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  NavLink
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 import SignUpForm from "./components/SignUpForm";
 import LogInForm from "./components/LogInForm";
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class SignUpForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      email: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    let target = e.target;
+    let value = target.type === "checkbox" ? target.checked : target.value;
+    let name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    //axios.post(URL);
+    console.log("The form was submitted with ther following data:");
+    console.log(this.state);
+  }
   render() {
     return (
       <div className="FormCenter">
-        <form className="FormFields" onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="FormFields">
           <div className="FormField">
             <label className="FormField__Label" htmlFor="name">
               Name
@@ -15,6 +46,8 @@ class SignUpForm extends Component {
               className="FormField__Input"
               placeholder="Enter your name"
               name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
             />
           </div>
 
@@ -28,6 +61,8 @@ class SignUpForm extends Component {
               className="FormField__Input"
               placeholder="Enter your email"
               name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
             />
           </div>
 
@@ -35,39 +70,36 @@ class SignUpForm extends Component {
             <label className="FormField__Label" htmlFor="genere">
               <p>Favorite movie genere</p>
             </label>
-            Action: <input type="checkbox" name="sports" value="soccer" />
+            Action: <input type="checkbox" name="genere" value="action" />
             <br />
-            Drama: <input type="checkbox" name="sports" value="football" />
+            Drama: <input type="checkbox" name="genere" value="drama" />
             <br />
-            Thriller: <input type="checkbox" name="sports" value="baseball" />
+            Thriller: <input type="checkbox" name="genere" value="thriller" />
             <br />
-            Comedy: <input type="checkbox" name="sports" value="basketball" />
+            Comedy: <input type="checkbox" name="genere" value="comedy" />
             <br />
-            Animation:{" "}
-            <input type="checkbox" name="sports" value="basketball" />
+            Animation: <input type="checkbox" name="genere" value="animation" />
             <br />
-            SciFi: <input type="checkbox" name="sports" value="basketball" />
+            SciFi: <input type="checkbox" name="genere" value="scifi" />
           </div>
 
           <div className="FormField">
-            <label className="FormField__Label" htmlFor="genere">
+            <label className="FormField__Label" htmlFor="decade">
               <p>Favorite movie decade</p>
             </label>
-            50-60: <input type="checkbox" name="sports" value="soccer" />
+            50s: <input type="checkbox" name="decade" value="50s" />
             <br />
-            60-70: <input type="checkbox" name="sports" value="football" />
+            60s: <input type="checkbox" name="decade" value="60s" />
             <br />
-            70-80: <input type="checkbox" name="sports" value="baseball" />
+            70s: <input type="checkbox" name="decade" value="70s" />
             <br />
-            80-90: <input type="checkbox" name="sports" value="basketball" />
+            80s: <input type="checkbox" name="decade" value="80s" />
             <br />
-            90-2000: <input type="checkbox" name="sports" value="basketball" />
+            90s: <input type="checkbox" name="decade" value="90s" />
             <br />
-            2000-2010:{" "}
-            <input type="checkbox" name="sports" value="basketball" />
+            2000s: <input type="checkbox" name="decade" value="2000s" />
             <br />
-            2010-2020:{" "}
-            <input type="checkbox" name="sports" value="basketball" />
+            2010s: <input type="checkbox" name="decade" value="2010s" />
           </div>
 
           <div className="FormField">
@@ -80,14 +112,16 @@ class SignUpForm extends Component {
               className="FormField__Input"
               placeholder="Enter your password"
               name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
             />
           </div>
 
           <div className="FormField">
             <button className="FormField__Button mr-20">Sign Up</button>{" "}
-            <a href="#" className="FormField__Link">
+            <Link to="/log-in" className="FormField__Link">
               I'm already registered
-            </a>
+            </Link>
           </div>
         </form>
       </div>
